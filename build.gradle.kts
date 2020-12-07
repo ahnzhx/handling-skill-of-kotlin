eb1611b5d87ed1f27bee07b488a8be602fb318e7
ahnzhx <modunaeggu@naver.com>
2020-12-08 07:57:11 +0900

    feat:setting environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
+    kotlin("plugin.spring") version "1.3.72"
+    kotlin("plugin.jpa") version "1.4.10"
+    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+
 }
 
 group = "me.sohyunahn"
@@ -12,7 +17,17 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("com.google.firebase:firebase-admin:6.16.0")
+
+    runtimeOnly("com.h2database:h2")
+
     testImplementation(kotlin("test-junit5"))
+    testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
